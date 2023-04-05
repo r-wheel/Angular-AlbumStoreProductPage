41a78207036f3c60c34c9494641bfa3ea142c549
Arwil Tinambacan <arwil.tnmbcn@gmail.com>
2023-04-05 15:00:33 +0800

    Summary

---
+++ src/app/product-description/product-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-description-page',
+  templateUrl: './product-page.description.html',
+  styleUrls: ['./product-page.description.css']
+})
+export class ProductDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
